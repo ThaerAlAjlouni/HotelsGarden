0e21ee0244042b3ec32467c2c75324cfee26f004
Tha'er Al-Ajlouni <thaer.m.ajlouni@outlook.com>
2018-01-13 02:17:21 +0200

    Add project files.

---
+++ HotelsGarden/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotelsGarden.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
